f6bee222cc47dc5ac02ee884aead6d379013d0dc
Matias Gonzalez <mng_epson@hotmail.com>
2023-02-09 17:56:42 -0300

    feat: add button and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Boton from "./components/Boton";
+import Logo from "./img/unnamed.png";
 
-function App() {
+const App = ()=> {
+  const manejarClick = () => {
+    console.log("Clic");
+  };
+
+  const reiniciarContador = () => {
+    console.log("Reiniciar");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
+          src={Logo}
+          alt="Logo de freecodecamp"
+        />
+      </div>
+      <div className="contenedor-contador">
+        <Boton
+          texto="Click"
+          esBotonDeClick={true}
+          manejarClick={manejarClick}
+        />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClick={false}
+          manejarClick={reiniciarContador}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Boton.js
@@ -0,0 +1,14 @@
+import React from 'react';  
+import '../styles/Boton.css';
+
+const Boton = ({texto, esBotonDeClick, manejarClick}) => {
+  return (
+    <button
+    className={esBotonDeClick ? 'boton-click' : 'boton-reiniciar'}
+    onClick={manejarClick}>
+      {texto}
+      </button>
+    )
+}
+
+export default Boton;
